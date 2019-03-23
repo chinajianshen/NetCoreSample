9737e6538e19aa2c47ebd7d47c4f60a27a4bed5b
chinajianshen <281349838@qq.com>
2019-03-23 10:06:59 +0800

    提交

--- WebApp2/VueStudy/Controllers/VueController.cs
+++ WebApp2/VueStudy/Controllers/VueController.cs
@@ -12,5 +12,87 @@ namespace VueStudy.Controllers
         {
             return View();
         }
+
+        public IActionResult VueIf()
+        {
+            return View();
+        }
+
+        public IActionResult VueCircle()
+        {
+            return View();
+        }
+
+        public IActionResult VueComputed()
+        {
+            return View();
+        }
+
+        public IActionResult VueWatch()
+        {
+            return View();
+        }
+
+        public IActionResult VueStyle()
+        {
+            return View();
+        }
+
+        public IActionResult VueEvent()
+        {
+            return View();
+        }
+
+        public IActionResult VueForm()
+        {
+            return View();
+        }
+
+        public IActionResult VueComponent()
+        {
+            return View();
+        }
+
+        public IActionResult VueCustomDirective()
+        {
+            return View();
+        }
+
+        public IActionResult VueRouting()
+        {
+            return View();
+        }
+
+        public IActionResult VueTransition()
+        {
+            return View();
+        }
+
+        public IActionResult VueMinin()
+        {
+            return View();
+        }
+
+        public IActionResult VueAjax()
+        {
+            return View();
+        }
+
+        public IActionResult GetData(int prm1,string prm2)
+        {
+            //int.Parse(prm2);
+            return Json(new { state = prm1, msg = prm2 });
+        }
+
+        [HttpPost]
+        public IActionResult PostData(int prm1, string prm2)
+        {
+            return Json(new { state = prm1, msg = prm2 });
+        }
+
+        public IActionResult VueReactive()
+        {
+            return View();
+        }
     }
 }
